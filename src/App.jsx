d9feba03bce8caa6c9dfb82d78bd2dc9b36007e1
unknown <diulioaires@outlook.com>
2025-04-10 19:29:53 -0300

    alterações para o app interpretar o código

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import "./App.css"
+import React from 'react';
+import './App.css';
+import Header from './components/Header.jsx';
+import Home from './components/home.jsx';
+import Footer from './components/Footer.jsx';
 
-function App(){
-    return(
-        <>
-    
-        </>
-    )
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.jsx';
+import reportWebVitals from './reportWebVitals.js';
+import Header from './components/Header';
+import Home from './components/home.jsx';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
